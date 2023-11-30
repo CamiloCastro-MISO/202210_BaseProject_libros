43e29703058491d619d6907a13983c571f2407c0
CamiloCastro-uniAndes <info@mobi-ai.com>
2023-11-29 22:32:53 -0500

    variable de ambiente del servicio

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // Importamos HttpClient
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-constructor(private http: HttpClient) { } // Inyectamos HttpClient
+  private apiUrl: string = environment.baseUrl + 'books';
+
+  constructor(private http: HttpClient) { } // Inyectamos HttpClient
 
 }
