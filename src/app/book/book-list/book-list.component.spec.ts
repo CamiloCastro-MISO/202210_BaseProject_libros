3df55ab7cac53c093a0a44b25a0aabbe4c1028aa
CamiloCastro-uniAndes <info@mobi-ai.com>
2023-11-29 23:30:22 -0500

    agregar el debug element y la creacion de datos aleatorios en la funcion flecha before each

--- src/app/book/book-list/book-list.component.spec.ts
+++ src/app/book/book-list/book-list.component.spec.ts
@@ -8,24 +8,45 @@ import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { BookService } from "../book.service";
 import { Book } from "../book";
 import { Editorial } from 'src/app/editorial/editorial';
+import { faker } from '@faker-js/faker';}
 
 describe('BookListComponent', () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [ BookListComponent ],
-      providers: [ BookService ]
+      declarations: [BookListComponent],
+      providers: [BookService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookListComponent);
     component = fixture.componentInstance;
+
+    const editorial = new Editorial(
+      faker.datatype.number(),
+      faker.lorem.sentence()
+    );
+
+    for (let i = 0; i < 10; i++) {
+      const book = new Book(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.image.imageUrl(),
+        faker.date.past(),
+        editorial,
+      );
+      component.books.push(book);
+    }
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
