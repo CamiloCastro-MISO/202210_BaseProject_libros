12d32f4586eccb46a8758ad3cbaaac934984a5e8
CamiloCastro-uniAndes <info@mobi-ai.com>
2023-11-29 22:13:30 -0500

    importamos el modulo en el modulo principal Appmodule

--- src/app/editorial/editorial.module.ts
+++ src/app/editorial/editorial.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EditorialComponent } from './editorial.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [EditorialComponent]
+  declarations: []
 })
 export class EditorialModule { }
