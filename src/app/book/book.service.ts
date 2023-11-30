f31517f3939b847efc75fd330f12fc217a50d8a0
CamiloCastro-uniAndes <info@mobi-ai.com>
2023-11-29 22:36:17 -0500

    creacion del observable y la funcion que tiene los datos del objecto observable que en este caso es el api

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // Importamos HttpClient
 import { environment } from '../../environments/environment';
+import { Book } from './book';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,9 @@ export class BookService {
   private apiUrl: string = environment.baseUrl + 'books';
 
   constructor(private http: HttpClient) { } // Inyectamos HttpClient
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
 
 }
