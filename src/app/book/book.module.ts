d137d93ab4c85ebf48d242b864be3fe0599031ea
CamiloCastro-uniAndes <info@mobi-ai.com>
2023-11-29 21:13:28 -0500

    borrar archivos innecesarios dado que usaremos el modulo para varios compoentes

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BookComponent } from './book.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BookComponent]
+  declarations: []
 })
 export class BookModule { }
