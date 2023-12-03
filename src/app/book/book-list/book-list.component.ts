e6ced0a43199c2bc378d58a9df21102f13a1496e
CamiloCastro-uniAndes <info@mobi-ai.com>
2023-12-03 02:47:10 -0500

    La función debe definirse en el componente y se ocupa de: Cambiar el valor al atributo selected

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -10,6 +10,7 @@ import { BookService } from '../book.service';
 export class BookListComponent implements OnInit {
 
   books: Array<Book> = [];
+  selectedBook!: Book;
   selected: Boolean = false; //Inicializamos la variable selected en false y luego cuando el usuario selecciona un libro, le cambiamos el valor a true.
 
   constructor(private bookService: BookService) { }
@@ -22,6 +23,11 @@ export class BookListComponent implements OnInit {
     this.bookService.getBooks().subscribe((books) => {
       this.books = books;
     });
+  }
+
+  onSelected(book: Book): void {
+    this.selected = true;
+    this.selectedBook = book;
   }
 
 }
