4a0964f2fc1d0ea5ee50f6fa1512afd4faf77d4c
CamiloCastro-uniAndes <info@mobi-ai.com>
2023-12-03 02:21:33 -0500

    Declarar el parámetro de entrada al componente detalle e incluye el signo de admiración de cierre en el nombre del atributo para evitar que tenga un valor indefinido por defecto

--- src/app/book/book-detail/book-detail.component.ts
+++ src/app/book/book-detail/book-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-book-detail',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-detail.component.css']
 })
 export class BookDetailComponent implements OnInit {
+
+  @Input() bookDetail!: Book;
 
   constructor() { }
 
